aa302f63b68600b5f344b5760d8e2bfb3c14d34f
François L. GIRARD <82939484+francoisgirard51@users.noreply.github.com>
2022-10-29 17:35:31 +0200

    Update two_sum.c

--- two_sum.c
+++ two_sum.c
@@ -6,19 +6,19 @@ Given an array of integers nums and an integer target, return indices of the two
 int* twoSum nums, int numsSize, int target, int* returnSize)
 {
     int i, j;
-    int *result = (int *)malloc(2 * sizeof(int));
+    int *res = (int *)malloc(2 * sizeof(int));
     for (i = 0; i < numsSize; i++)
     {
         for (j = i + 1; j < numsSize; j++)
         {
             if (nums[i] + nums[j] == target)
             {
-                result[0] = i;
-                result[1] = j;
+                res[0] = i;
+                res[1] = j;
                 *returnSize = 2;
-                return result;
+                return res;
             }
         }
     }
-    return result;
+    return res;
 }
